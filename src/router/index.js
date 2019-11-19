9bc57349b3435c51aa753a5a59ea16b14069c463
Alexander Zetterberg <alexander.zetterberg@se360.se>
2019-11-19 17:43:23 +0100

    BAC-0002: Lint/Prettier config change, Naming change, Jest setup

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,25 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-import Setup from "../views/Setup.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Calendar from '../views/Calendar.vue';
+import Setup from '../views/Setup.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "setup",
-    component: Setup
-  },
-  {
-    path: "/Calendar",
-    name: "Calendar",
-    component: Home
-  }
+    {
+        path: '/',
+        name: 'setup',
+        component: Setup
+    },
+    {
+        path: '/Calendar',
+        name: 'Calendar',
+        component: Calendar
+    }
 ];
 
 const router = new VueRouter({
-  routes
+    routes
 });
 
 export default router;
