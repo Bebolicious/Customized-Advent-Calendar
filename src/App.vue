b1718d13d204e3887bb1a8cab985e008b7fc6907
Alexander "Bebo <44731083+Bebolicious@users.noreply.github.com>
2019-11-21 09:57:44 +0100

    BAC-0002-EXTENDED: Add forms, new prettier config.  (#3)
    * BAC-0002: Lint/Prettier config change, Naming change, Jest setup
    
    * BAC-0002-EXTENDED: Small changes, added json write functionality

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <v-app>
-    <v-content>
-      <router-view />
-    </v-content>
-  </v-app>
+    <v-app>
+        <v-content>
+            <router-view />
+        </v-content>
+    </v-app>
 </template>
 
 <script>
 export default {
-  name: "App",
-  data() {
-    return {
-      //
-    };
-  }
+    name: 'App',
+    data() {
+        return {
+            //
+        };
+    }
 };
 </script>
 
 <style>
 html {
-  height: 100%;
-  overflow: hidden;
-  background-color: #223355
+    height: 100%;
+    overflow: hidden;
+    background-color: #223355;
 }
 </style>
--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -1,42 +1,59 @@
 <template>
-  <div id="Setup">
-    <v-form class="bform">
-      <v-container>
-        <v-text-field class="text-xs-center" v-model="name" label="Namn" required></v-text-field>
-        <v-text-field class="text-xl-center" v-model="calendarOptions" label="Luckinnehåll" required></v-text-field>
-        <v-btn color="success" class="mr-4" @click="toCalendar">Gå vidare</v-btn>
-      </v-container>
-    </v-form>
+    <div id="Setup">
+        <v-form class="bform">
+            <v-container>
+                <v-text-field class="text-xs-center" v-model="name" label="Namn" required></v-text-field>
+                <v-text-field
+                    class="text-xl-center"
+                    v-model="calendarOptions"
+                    label="Luckinnehåll"
+                    required
+                ></v-text-field>
+                <v-btn color="success" class="mr-4" @click="saveAndContinueToCalendar">Gå vidare</v-btn>
+            </v-container>
+        </v-form>
 
-    <pre>{{ $data || json }}</pre>
-  </div>
+        <pre>{{ $data || json }}</pre>
+    </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      name: "",
-      calendarOptions: ""
-    };
-  },
-  methods: {
-    toCalendar: function() {
-      this.$router.push("./Calendar");
+    data() {
+        return {
+            name: '',
+            calendarOptions: ''
+        };
+    },
+    methods: {
+        saveAndContinueToCalendar: function() {
+            var fs = require('fs');
+            let calenderFormat = this.calendarOptions.split('#');
+            var json = JSON.stringify(calenderFormat);
+            fs.writeFile('myjsonfile.json', json, 'utf8', function(err, result) {
+                if (err) {
+                    // eslint-disable-next-line
+                    console.log('error', err);
+                } else {
+                    // eslint-disable-next-line
+                    console.log(result);
+                }
+            });
+            this.$router.push('./Calendar');
+        }
+    },
+    mounted() {
+        if (this.name !== '') {
+            this.$router.push('./Calendar');
+        }
     }
-  },
-  mounted() {
-    if (this.name !== "") {
-      this.$router.push("./Calendar");
-    }
-  }
 };
 </script>
 <style>
 #Setup {
-  background-color: #999999;
-  text-align: -webkit-center;
+    background-color: #999999;
+    text-align: -webkit-center;
 }
 .bform {
-  width: 20%;
+    width: 20%;
 }
 </style>
